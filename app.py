f0f3c76cc05f391458c19430a3c8d88fa464060a
Sayan <sgdgp.2014@gmail.com>
2018-09-16 12:23:13 +0530

    homepage

--- app.py
+++ app.py
@@ -283,39 +283,39 @@ def new_comment(variable1, variable2):
 	return render_template('article.html',headline=headline, commentPass=zip(comment_text_list,commentor_name_list), toPass=zip(id_list,para_text_list),  all_comment_text=comments)
 
 
-# @app.route("/post")
-# def post():
-# 	# load paragraph json
-# 	id_list, para_text_list = load_article("./static/data/article/test_article.json")
-# 	comments = load_comments("./static/data/comments/test_comments.json")
-# 	# id_list = ["4","5"]
-# 	# para_text_list = ["asdas","|wqewqeweq"]
-# 	return render_template('post.html', toPass=zip(id_list,para_text_list),  all_comment_text=comments)
+@app.route("/post")
+def post():
+	# load paragraph json
+	id_list, para_text_list = load_article("./static/data/article/test_article.json")
+	comments = load_comments("./static/data/comments/test_comments.json")
+	# id_list = ["4","5"]
+	# para_text_list = ["asdas","|wqewqeweq"]
+	return render_template('post.html', toPass=zip(id_list,para_text_list),  all_comment_text=comments)
 
 
-# @app.route("/index")
-# def index():
-# 	return render_template('index.html')
+@app.route("/index")
+def index():
+	return render_template('index.html')
 
-# @app.route('/', methods=['POST'])
-# def my_form_post():
-	# text = request.form['text']
-	# processed_text = str(text)
-	# processed_text = processed_text.strip()
-	# print(processed_text)
+@app.route('/', methods=['POST'])
+def my_form_post():
+	text = request.form['text']
+	processed_text = str(text)
+	processed_text = processed_text.strip()
+	print(processed_text)
 
-	# load the model here
+	load the model here
 	
-	# predict score for all paragraphs using the model
+	predict score for all paragraphs using the model
 
 	
-	# get the paragraph with the highest score
+	get the paragraph with the highest score
 
-	# append the comment for that section
+	append the comment for that section
 
-	# id_list, para_text_list = load_article("./static/data/article/test_article.json")
-	# comments = load_comments("./static/data/comments/test_comments.json")
-	# return render_template('index.html', toPass=zip(id_list,para_text_list),  all_comment_text=comments)
+	id_list, para_text_list = load_article("./static/data/article/test_article.json")
+	comments = load_comments("./static/data/comments/test_comments.json")
+	return render_template('index.html', toPass=zip(id_list,para_text_list),  all_comment_text=comments)
 
 if __name__ == "__main__":
 	app.secret_key = 'nips_cnerg'

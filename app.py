66dc05300db72ca667ae32a31d5f0ffd4520ecba
Sayan <sgdgp.2014@gmail.com>
2018-09-16 13:03:49 +0530

    index

--- app.py
+++ app.py
@@ -107,18 +107,18 @@ def get_input_vec(v1, v2):
 		e[i] /= sum
 
 
-@app.context_processor
-def override_url_for():
-    return dict(url_for=dated_url_for)
+# @app.context_processor
+# def override_url_for():
+#     return dict(url_for=dated_url_for)
 
-def dated_url_for(endpoint, **values):
-    if endpoint == 'static':
-        filename = values.get('filename', None)
-        if filename:
-            file_path = os.path.join(app.root_path,
-                                 endpoint, filename)
-            values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)
+# def dated_url_for(endpoint, **values):
+#     if endpoint == 'static':
+#         filename = values.get('filename', None)
+#         if filename:
+#             file_path = os.path.join(app.root_path,
+#                                  endpoint, filename)
+#             values['q'] = int(os.stat(file_path).st_mtime)
+#     return url_for(endpoint, **values)
 
 
 def load_article(filepath):
